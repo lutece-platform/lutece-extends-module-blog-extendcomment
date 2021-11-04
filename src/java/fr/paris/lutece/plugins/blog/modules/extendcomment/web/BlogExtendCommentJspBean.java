ecbf90811e14185db426afe340264a3d3c91f5b4
FrancoisEricMerlin <FrancoisEricMerlin>
2021-11-04 13:07:06 +0100

    BGEXTDCOMT-3 - Modify import of commons Lang

--- src/java/fr/paris/lutece/plugins/blog/modules/extendcomment/web/BlogExtendCommentJspBean.java
+++ src/java/fr/paris/lutece/plugins/blog/modules/extendcomment/web/BlogExtendCommentJspBean.java
@@ -41,7 +41,7 @@ import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import fr.paris.lutece.plugins.blog.business.Blog;
 import fr.paris.lutece.plugins.blog.business.BlogSearchFilter;
